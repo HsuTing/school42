04a87a39ee8e9c2cdd00a46313ddacb32b64765d
HsuTing <hsuting0106@gmail.com>
2022-10-01 02:58:40 +0000

    feat(school42): add main command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::{crate_version, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("school42")
+        .version(crate_version!())
+        .about("Some commands are used for the school42")
+        .subcommand_required(true)
+        .get_matches();
+
+    match matches.subcommand() {
+        _ => unreachable!(),
+    }
 }
